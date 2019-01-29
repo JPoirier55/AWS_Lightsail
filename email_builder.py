52f920352e98ffb03218ed6fcd74f2a4c1d5cb97
JPoirier55 <jake.poirier55@gmail.com>
2019-01-29 11:59:13 -0500

    Lil quick fix

--- email_builder.py
+++ email_builder.py
@@ -2,6 +2,12 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import smtplib
 
+t = ['THIS IS A TEST', 'Creating: Project_Blue-1_29_2019_H10-M56', 'Deleting: Project_Blue-12-19-2018',
+     'Creating: Moodle-1_29_2019_H10-M56', 'Deleting: Moodle-just_built_1_23_19',
+     'Creating: Node_sandbox-1_29_2019_H10-M56', 'Deleting: Node_sandbox-12-19-2018',
+     'Creating: AXIINTER_Webtools-1_29_2019_H10-M56', 'Deleting: AXIINTER_Webtools-12-19-2018',
+     'Creating: AWS_Backup_Handler-1_29_2019_H10-M56', 'Deleting: AWS_Backup_Handler-12-19-2018',
+     'Creating: WordPress-testbox-1_29_2019_H10-M56', 'Deleting: WordPress-testbox-12-19-2018']
 
 def build_html_message(message_arr):
     html = '<table cellpadding="0" cellspacing="0" border="0" width="100%" ' \
@@ -43,5 +49,4 @@ def sendEmail(subject, recipients, msgText):
     smtp.close()
 
 if __name__ == "__main__":
-    build_html_message(t)
-    # sendEmail('t', ['jake.poirier@axi-international.com'], build_html_message(t))
+    sendEmail('t', ['jake.poirier@axi-international.com'], build_html_message(t))
